04b4b14e597825d8ef05bc93483560cef0a0c1e9
Dark Matters FTC <barry@ruthandbarry.com>
2024-03-24 12:05:46 -0400

    Update streamlit_app.py
    correct panda

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 # st.stop()
 
 # convert the sp dataframe to panda df
-pd_df = my_datafrom.to_pandas()
+pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
